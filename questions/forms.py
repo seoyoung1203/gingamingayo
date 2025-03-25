88300967e27c143fbcfdf3589cf4ebed7c9d2eec
seoyoung <1203sylim@gmail.com>
2025-03-25 20:25:56 +0900

    update

--- questions/forms.py
+++ questions/forms.py
@@ -4,4 +4,4 @@ from .models import Question
 class QuestionForm(ModelForm):
     class Meta():
         model = Question
-        field = '__all__'
+        fields = '__all__'
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Question
+from .forms import QuestionForm
 # Create your views here.
+
+def index(request):
+    questions = Question.objects.all()
+    context = {
+        'questions' : questions,
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+    
+    if request.method == 'POST':
+        form = QuestionForm(request.POST) 
+        if form.is_valid:
+            form.save()
+            return redirect('questions:index')
+       
+    else:
+        forms = QuestionForm()
+
+    context = {
+        'forms' : forms,
+    }
+
+    return render(request, 'create.html', context)
